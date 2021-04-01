fbf1af966c791f9981e2d37afd0a084e259f4576
asefvanommeren <79085391+asefvanommeren@users.noreply.github.com>
2021-04-01 14:02:01 +0200

    + Mapbox API
    HTML en JS

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+mapboxgl.accessToken = 'pk.eyJ1IjoiYXNlZnZhbm9tbWVyZW4iLCJhIjoiY2ttbHRyNXM0MWVsajJvbnphaWJkN2J1byJ9.XJIUHE1YOYgZrzTVCXB5eA';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v11'
+});
