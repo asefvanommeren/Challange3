7c2e895b600cdf1604b3e588ae4115bb76f26acf
asefvanommeren <79085391+asefvanommeren@users.noreply.github.com>
2021-04-07 19:51:31 +0200

    + Mapbox Geocoding

--- scripts/script.js
+++ scripts/script.js
@@ -4,46 +4,70 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYXNlZnZhbm9tbWVyZW4iLCJhIjoiY2ttbHRyNXM0MWVsa
 var map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/streets-v11',
-  center: [4.30965, 52.080329], //Long, Lat
-  zoom: 11
+  center: [5.5759651, 52.0404333], //Long, Lat
+  zoom: 5
+});
+
+var bob = new MapboxGeocoder({
+  accessToken: mapboxgl.accessToken,
+  mapboxgl: mapboxgl
 });
 
 map.addControl(
-  new MapboxGeocoder({
-    accessToken: mapboxgl.accessToken,
-    mapboxgl: mapboxgl
-  })
+  bob
 );
 
-// ZOEKBALK
-var searchInput = document.getElementById(“searchbar_plaats”);
+// var ll =
+//   map.on('click', function (e) {
+//   document.getElementById('div1').innerHTML =
+//     // e.lngLat is the longitude, latitude geographical position of the event
+//     // JSON.stringify(e.lngLat.wrap());
+//     JSON.stringify(e.lngLat.wrap());
+//     //console.log(e.lngLat.lat);
+// });
 
-searchInput.onkeyup = zoekbalk;
+document.getElementsByClassName('mapboxgl-ctrl-geocoder--input')[0].onchange = function(){
+  //  alert('hoi');
+  // console.log(bob.mapMarker._lngLat.lat)
+  document.getElementById('div1-data').innerHTML = Number(bob.mapMarker._lngLat.lng) + ' | ' + Number(bob.mapMarker._lngLat.lat);
+    console.log(bob.mapMarker._lngLat.lng);
+    console.log(bob.mapMarker._lngLat.lat);
+};
 
-function zoekbalk() {
 
-  var searchValue =         document.getElementById(‘searchbar_plaats’).value.toUpperCase();
 
-  var table = document.getElementById(‘gottable’);
-  var allCharacters = table.getElementsByTagName(‘tr’);
+//
+// var locatie = new mapboxgl.GeolocateControl();
+//
+// map.addControl(locatie);
+//
+// locatie.on('locatie', function(e) {
+//   var lon = e.coords.longtitude;
+//   var lat = e.coords.latitude
+//   var positie = [lon, lat];
+//   console.log(positie);
+// });
 
-   for (var i=1; i < allCharacters.length; i++) {
-          var cellData = allCharacters[i].getElementsByTagName(‘td’)
 
-          for (var j=0; j < cellData.length; j++) {
-                  if(cellData[j].innerHTML.toUpperCase().indexOf(searchValue) > -1) {
-                           var hit = true;
-                  }
-          }
 
-          if(hit) {
-                  allCharacters[i].style.display = “”;
-                   hit = false;
-          }
-          else {
-                  if(i>0){
-                          allCharacters[i].style.display = “none”;
-                  }
-          }
-  }
-}
+
+
+
+
+// GEBRUIKERS LOCATIE
+// https://docs.mapbox.com/mapbox-gl-js/api/markers/#geolocatecontrol.event:geolocate
+
+// var geolocate = new mapboxgl.GeolocateControl({
+//   positionOptions: {
+//     enableHighAccuracy: true
+//   },
+//   trackUserLocation: true
+// });
+//
+// // Add the control to the map.
+// map.addControl(geolocate);
+// // Set an event listener that fires
+// // when a geolocate event occurs.
+// geolocate.on('geolocate', function() {
+//   console.log('A geolocate event has occurred.')
+// });
